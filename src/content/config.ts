bff29fe1b4c984aa52c1ee3ebec24d6d681347f3
pop-thanit <67604669+pop-thanit@users.noreply.github.com>
2025-12-18 16:32:32 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-const postsCollection = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			pubDate: z.date(),
-			description: z.string(),
-			link: z.optional(z.string()),
-			video: z.optional(z.string()),
-			author: z.object({
-				name: z.string(),
-				link: z.string(),
-			}),
-			image: z.object({
-				source: image(),
-				alt: z.string(),
-			}),
-			tags: z.array(z.string()),
-		}),
-});
-
 const projectsCollection = defineCollection({
 	schema: ({ image }) =>
 		z.object({
@@ -43,20 +23,6 @@ const projectsCollection = defineCollection({
 		}),
 });
 
-const authorsCollection = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			name: z.string(),
-			description: z.string(),
-			image: z.object({
-				source: image(),
-				alt: z.string(),
-			}),
-		}),
-});
-
 export const collections = {
-	posts: postsCollection,
 	projects: projectsCollection,
-	authors: authorsCollection,
 };
